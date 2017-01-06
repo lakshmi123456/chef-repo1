a55826852fc08e85d32d453c6b5b23f771a78f71
root <root@localhost.localdomain>
2017-01-06 09:28:51 +0000

    crt chef-repo

---
+++ chef-repo/chef-repo/cookbooks/sample/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: sample
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+file 'login.txt' do
+action:create
+content 'this is file' 
+end
+
+
+
